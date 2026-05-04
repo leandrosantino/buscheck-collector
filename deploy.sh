8dceec91a52cafa9b1b161a320a97cf635ae2664
leandrosantino <leandrosantino2013@gmail.com>
2026-05-03 21:54:31 -0300

    feat: Criado script para hospedar servidor

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+# scp -i ~/dev/OCI/LAB01.key ./dist/main ubuntu@137.131.151.245:~/main
+# scp -i ~/dev/OCI/LAB01.key ./.env ubuntu@137.131.151.245:~/.env
+
+# nohup ~/buscheck-collector > ~/buscheck-collector.log 2>&1 &
+
+# PID: 116949
